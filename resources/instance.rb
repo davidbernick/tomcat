b46957921524086eeaf1f5d918c913c90f90d6a0
David Bernick <dbernick@gmail.com>
2015-07-02 11:41:04 -0400

    Update instance.rb

--- resources/instance.rb
+++ resources/instance.rb
@@ -17,11 +17,17 @@ attribute :ssl_proxy_port,
   :kind_of => Fixnum
 attribute :ajp_port,
   :kind_of => Fixnum
+attribute :ajp_packetsize,
+  :kind_of => Fixnum
+
 attribute :shutdown_port,
   :kind_of => Fixnum,
   :required => true
 
 # The rest will inherit from node attributes
+attribute :uriencoding,
+  :kind_of => String
+
 attribute :config_dir,
   :kind_of => String
 attribute :log_dir,
